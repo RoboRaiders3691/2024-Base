1956b6e04f1e055aef23d1cfc0866f1710014c15
RoboRaiders3691 <northfieldfrc3691@gmail.com>
2024-02-17 11:34:51 -0600

    limelight update

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
-#include "LimelightHelpers.h"
+//#include "LimelightHelpers.h"
 
 #include <fmt/core.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -65,7 +65,7 @@ void Robot::RobotPeriodic() {
 
   // measurements.
 
-  frc::Pose3d visionMeasurement3d = ObjectToRobotPose(
+  /*frc::Pose3d visionMeasurement3d = ObjectToRobotPose(
 
       m_objectInField, m_robotToCamera, m_cameraToObjectEntryRef
       );
@@ -86,7 +86,7 @@ void Robot::RobotPeriodic() {
 
   m_poseEstimator.AddVisionMeasurement(visionMeasurement2d,
 
-                                       frc::Timer::GetFPGATimestamp());
+                                       frc::Timer::GetFPGATimestamp());*/
 
   frc::Pose2d getpose;
   m_field.SetRobotPose(getpose);
